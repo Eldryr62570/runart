b5d3b4449ab62ea31e7865584622f11093e3f132
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-06-23 11:24:10 +0200

    tags crid

--- src/Controller/Admin/OeuvreCrudController.php
+++ src/Controller/Admin/OeuvreCrudController.php
@@ -3,7 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Oeuvre;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 
 class OeuvreCrudController extends AbstractCrudController
 {
@@ -12,14 +18,19 @@ class OeuvreCrudController extends AbstractCrudController
         return Oeuvre::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            ImageField::new('photo_oeuvre')->setLabel("Photo oeuvre")
+            ->setUploadDir('public/build/uploads')
+            ->setUploadedFileNamePattern('/uploads/[contenthash].[extension]'),
+            TextField::new('nom_oeuvre'),
+            NumberField::new('prix'),
+            TextField::new('description_oeuvre'),
+            AssociationField::new('users')
         ];
     }
-    */
+    
 }
