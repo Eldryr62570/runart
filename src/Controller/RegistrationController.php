fe922e13f564f17bdfae9c2377c0250e24fc6342
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-07-22 09:25:55 +0200

    Fix flashes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -44,7 +44,7 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // generate a signed url and email it to the user
+            //Génére un email de vérification
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
                     ->from(new Address('runart-not-reply@gmail.com', 'MailBot'))
@@ -52,8 +52,10 @@ class RegistrationController extends AbstractController
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
-            // do anything else you need here, like send an email
-
+            $this->addFlash(
+                'success',
+                'Inscription terminée ! Veuillez vous connectez'
+            );
             return $this->redirectToRoute('app_login');
         }
 
